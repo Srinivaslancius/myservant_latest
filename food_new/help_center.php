bd4819aff894acaaa414394b577b44efa08cf181
srinivaslancuius <srinivas@lanciussolutions.com>
2017-12-25 11:41:13 +0530

    changes in footre file

---
+++ food_new/help_center.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html style="overflow-x:hidden">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <?php include_once './meta_fav.php';?>
+    <!-- GOOGLE WEB FONT -->
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,700,900,400italic,700italic,300,300italic' rel='stylesheet' type='text/css'>
+
+    <!-- BASE CSS -->
+    <link href="css/base.css" rel="stylesheet">
+
+		
+    
+    <!-- SPECIFIC CSS -->
+    <link href="layerslider/css/layerslider.css" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.min.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+<!--[if lte IE 8]>
+    <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a>.</p>
+<![endif]-->
+
+	<div id="preloader">
+        <div class="sk-spinner sk-spinner-wave" id="status">
+            <div class="sk-rect1"></div>
+            <div class="sk-rect2"></div>
+            <div class="sk-rect3"></div>
+            <div class="sk-rect4"></div>
+            <div class="sk-rect5"></div>
+        </div>
+    </div><!-- End Preload -->
+
+    <!-- Header ================================================== -->
+    <header>
+	  <?php include_once './header.php';?>
+        </header>
+    <!-- End Header =============================================== -->
+<?php $getAllHelpCenterData = getAllDataWhere('food_content_pages','id',15);
+          $getHelpCenterData = $getAllHelpCenterData->fetch_assoc();
+?>
+<!-- SubHeader =============================================== -->
+<section class="parallax-window" id="short" data-parallax="scroll" data-image-src="img/sub_header_home.jpg" data-natural-width="1400" data-natural-height="350">
+    <div id="subheader">
+    	<div id="sub_content">
+    	 <h1><?php echo $getHelpCenterData['title']; ?></h1>
+         <p><?php echo $getHelpCenterData['meta_title']; ?></p>
+         <p></p>
+        </div><!-- End sub_content -->
+	</div><!-- End subheader -->
+</section><!-- End section -->
+<!-- End SubHeader ============================================ -->
+
+    <div id="position">
+        <div class="container">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><?php echo $getHelpCenterData['title']; ?></li>
+            </ul>
+            <a href="#0" class="search-overlay-menu-btn"><i class="icon-search-6"></i> Search</a>
+        </div>
+    </div><!-- Position -->
+
+<!-- Content ================================================== -->
+<div class="container margin_60_35">
+    <div class="main_title margin_mobile">
+            <h2 class="nomargin_top"><?php echo $getHelpCenterData['title']; ?></h2>
+        </div>	
+			<div class="feature_2">
+				<?php echo $getHelpCenterData['description']; ?>
+			</div>
+		</div>
+	<div class="high_light">
+       <?php include_once 'view_restaurants.php'; ?>
+      </div>
+	  
+	  <!-- Footer ================================================== -->
+	<footer>
+         <?php include_once 'footer.php'; ?>
+		 </footer>
+<!-- End Footer =============================================== -->
+
+<div class="layer"></div><!-- Mobile menu overlay mask -->
+
+<!-- Login modal -->   
+
+    
+     <!-- Search Menu -->
+	<div class="search-overlay-menu">
+		<span class="search-overlay-close"><i class="icon_close"></i></span>
+		<form role="search" id="searchform" method="get">
+			<input value="" name="q" type="search" placeholder="Search..." />
+			<button type="submit"><i class="icon-search-6"></i>
+			</button>
+		</form>
+	</div>
+	<!-- End Search Menu -->
+    
+<!-- COMMON SCRIPTS -->
+<script src="js/jquery-2.2.4.min.js"></script>
+<script src="js/common_scripts_min.js"></script>
+<script src="js/functions.js"></script>
+<script src="assets/validate.js"></script>
+
+</body>
+</html>
