8120ab1bd3dd9082196711eda1132adb8f63cc03
Srinivaslancius <srinivas@lanciussolutions.com>
2017-12-26 20:26:09 +0530

    new food chanegs

--- food_new/food_manage_webmaster/admin_includes/footer.php
+++ food_new/food_manage_webmaster/admin_includes/footer.php
@@ -1,9 +1,8 @@
     <div class="site-footer"> Design & Developed By Lancius IT Solutions</div> 
     </div>
-    <script src="js/vendor.min.js"></script>
-    <script src="js/cosmos.min.js"></script>
+    
     <script src="js/application.min.js"></script>
-    <script src="js/index.min.js"></script>
+   
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <script>
       $( function() {
