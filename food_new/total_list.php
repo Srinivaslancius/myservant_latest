be7aacb133b3a67973824628240bc80c95d6295c
Srinivaslancius <srinivas@lanciussolutions.com>
2017-12-27 19:06:22 +0530

    food changes

--- food_new/total_list.php
+++ food_new/total_list.php
@@ -2,11 +2,11 @@
 include "../admin_includes/config.php";
 include "../admin_includes/food_common_functions.php";
 include "../admin_includes/common_functions.php";
-$getRestaurants = getAllRestaruntsWithProducts('0','','');;
+$getRestaurants = getAllRestaruntsWithProducts('0','','');
 while($getRestaurantsData = $getRestaurants->fetch_assoc()) {
  $img = $base_url . 'uploads/food_vendor_logo/'.$getRestaurantsData['logo']; 
  $output = '';
- $output .=  '<div class="col-md-6 filter_data">
+ $output .=  ' <input type="hidden" id="get_res_cnt" value='.$getRestaurants->num_rows .'><div class="col-md-6 filter_data">
                 <div class="strip_list wow fadeIn" data-wow-delay="0.1s">
                         <div class="row">
                                 <div class="col-md-8 col-sm-9">
