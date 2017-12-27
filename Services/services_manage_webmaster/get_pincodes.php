3c0a1a44a457b437d7ea2d7aebe4126e161a7338
srinivaslancius <srinivas@lanciussolutions.com>
2017-12-27 17:00:29 +0530

    changed index page

--- Services/services_manage_webmaster/get_pincodes.php
+++ Services/services_manage_webmaster/get_pincodes.php
@@ -2,7 +2,7 @@
 include_once('../../admin_includes/config.php');
 include_once('../../admin_includes/common_functions.php');
 if(!empty($_POST["lkp_city_id"])) {
-	$query ="SELECT * FROM lkp_pincodes WHERE lkp_status_id = 0 AND lkp_city_id = '" . $_POST["lkp_city_id"] . "' AND id IN (SELECT pincodes FROM availability_of_locations WHERE lkp_status_id = 0)";
+	$query ="SELECT * FROM lkp_pincodes WHERE lkp_status_id = 0 AND lkp_city_id = '" . $_POST["lkp_city_id"] . "' ";
 	$results = $conn->query($query);
 ?>
 	<option value="">Select Pincode</option>
