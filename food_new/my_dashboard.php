2ca8da366781c81f87dfbfdbd3fe2b60e2a59866
Srinivaslancius <srinivas@lanciussolutions.com>
2017-12-29 17:32:18 +0530

    done design changes

--- food_new/my_dashboard.php
+++ food_new/my_dashboard.php
@@ -143,8 +143,7 @@ ul#cat_nav li a#active {
             			<td>2017-12-21 12:18:45</td>
             			<td>Rs.1499</td>
             			<td>some one</td>
-            			<td>MYSER-SERVICESzvj760171221121845
-            			
+            			<td>MYSER-FOODuwv
             			</td>
             		  </tr>
             		  <tr>
@@ -153,14 +152,13 @@ ul#cat_nav li a#active {
             			<td></td>
             			<td>
             			<div class="row">
-            			<div class="col-sm-3 col-xs-4">
-            			<button class="button1">Details</button>
+            			<div class="col-sm-4 col-xs-4">
+            			<center><button class="button1">Details</button></center>
             			</div>
-            			<div class="col-sm-5 col-xs-8">
+            			<div class="col-sm-8 col-xs-8">
             			<button class="button1 button2">Track</button>
             			</div>
-            			<div class="col-sm-4">
-            			</div>
+            			
             			</div>
             			</td>
             		  </tr>
