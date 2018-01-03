8b641982705e1121470d5fd7e9e484b6f7c4b91d
Srinivaslancius <srinivas@lanciussolutions.com>
2018-01-03 14:09:34 +0530

    cart page chnaghes

--- Services/services_mobile_app/sub_categories.php
+++ Services/services_mobile_app/sub_categories.php
@@ -12,6 +12,18 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 		$result = getAllDataWhereWithActive('services_sub_category','services_category_id',$_REQUEST['categoryId']);
 		if ($result->num_rows > 0) {
 				$response["lists"] = array();
+
+				$cat_id=$_REQUEST['categoryId'];
+		    	$getBanners1 = "SELECT * FROM `services_banners` WHERE lkp_status_id = 0 ANd service_category_id = $cat_id ORDER BY id DESC";
+			    $getBanners = $conn->query($getBanners1);
+			    $getAllBanners = $getBanners->fetch_assoc();
+
+			    if(!empty($getAllBanners['banner'])) {
+			    	$response["categoryBannerImage"] = $base_url."uploads/services_banner_images/".$getAllBanners["banner"];
+			    } else {
+			    	$response["categoryBannerImage"] = $base_url."Services/img/slides/slide_1.jpg";
+			    }
+				    
 				while($row = $result->fetch_assoc()) {
 					//Chedck the condioton for emptty or not		
 					$lists = array();
