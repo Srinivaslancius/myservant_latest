d166109f775da5aef1aefcb7fb952a9b5961735d
Srinivaslancius <srinivas@lanciussolutions.com>
2018-01-05 13:04:31 +0530

    done design changes

--- Services/thankyou.php
+++ Services/thankyou.php
@@ -23,6 +23,40 @@
         <link href="site_launch/css/style.css" rel="stylesheet">
     <link href="layerslider/css/layerslider.css" rel="stylesheet">
     <!-- REVOLUTION SLIDER CSS -->
+	<style>
+	.table>thead>tr>th {
+    vertical-align: bottom;
+    border-bottom:1px;
+	background-color:#f1f1f1;
+	
+}
+.table>tbody>tr>td, .table>tbody>tr>th, .table>tfoot>tr>td, .table>tfoot>tr>th, .table>thead>tr>td, .table>thead>tr>th {
+    padding: 10px;
+    line-height: 1.42857143;
+    vertical-align: top;
+   border-top: 1px solid #ddd;
+  
+}
+.table>tbody>tr>td{
+	border-bottom:0px;
+}
+	.table {
+     border: 1px solid #ddd;
+}
+.box_style_1{
+	border:0px;
+	padding:0px;
+}
+.box_style_1 h3.inner{
+	background-color:#fe6003;
+}
+.box_style_1 h3.inner {
+    margin: -30px -31px 30px;
+	height:50px;
+	
+}
+
+	</style>
 </head>
 
 <body>
@@ -88,16 +122,73 @@
             <div class="col-sm-3"></div>
            <div class="col-sm-6">
                     <div id="login">
-                            <div class="text-center"><h2><strong>Thank<span> You</span></strong></h2>
-                            <hr>
+					<div class="box_style_1">
+						<h3 class="inner" style="text-align:left">Order Confirmed!</h3>
+						
+					<center><span class="icon-ok-circled2" style="color:#fe6003;;font-size:150px;font-weight:normal"></span></center>
+					
+                           <div class="text-center"><h2 style="color:#333"><strong>Thank You!</strong></h2>
+						   <p style="text-align:center">Your Order No is: <b>MYSER-SERVICESqde043</b><br>
+							You will be redirected to the Home in 10 seconds.<br>
+						   <b>Delivery Address:</b> Madhapur, Hyderabad</p>
+                            <!--<hr>
                             <span class="icon-ok" style="color:green;font-size:100px;"></span>
                             <p style="text-align:center"><b>Your order has been received</b></p>
                             <p style="text-align:center">Your Order is: <strong><?php echo $orderId; ?></strong></p>
                             <p style="text-align:center">Billing & Shipping Information: <?php echo $getservicesOrders['address']; ?></p>
-                            <p style="text-align:center">You will be redirected to the Home in 10 seconds.</p>  
-                        </div>
-                        </div>
-                </div>
+                            <p style="text-align:center">You will be redirected to the Home in 10 seconds.</p>-->
+							
+                        </div><h4>Summary</h4>
+						<table class="table table-striped">
+						<thead>
+						  <tr>
+							<th>Firstname</th>
+							<th>Lastname</th>
+							<th>Email</th>
+						  </tr>
+						</thead>
+						<tbody>
+						  <tr style="background-color:white;">
+							<td><p>Sub Total</p>
+							<p>Delivery fee</p>
+							<p>Service Tax</p></td>
+							<td></td>
+							<td><p>Rs.80</p>
+							<p>Rs.50</p>
+							<p>Rs.8(10%)</p></td>
+						  </tr>						 
+						</tbody>
+						</table>
+						<table class="table table-striped" style="border:0px">
+						<thead>
+						  <tr>
+							<th>Firstname</th>
+							<th>Lastname</th>
+							<th>Email</th>
+						  </tr>
+						</thead>
+						<tbody>
+						  <tr>
+							<td>Sub Total</td>
+							<td></td>
+							<td>Rs.80</td>
+							</tr>
+							<tr>
+							<td>Delivery fee</td>
+							<td></td>
+							<td>Rs.50</td>
+							</tr>
+							<tr>
+							<td>Service Tax</td>
+							<td></td>
+							<td>Rs.8(10%)</td>
+							
+						  </tr>						 
+						</tbody>
+						</table>
+						
+                    </div>
+                </div></div>
             <div class="col-sm-3"></div>    
             
                 <div class="col-sm-1">
