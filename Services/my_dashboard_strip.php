12a81cf88d754a1b508b8b74079d09e000c3309e
Srinivaslancius <srinivas@lanciussolutions.com>
2018-01-08 14:54:48 +0530

    done design changes

--- Services/my_dashboard_strip.php
+++ Services/my_dashboard_strip.php
@@ -1,6 +1,11 @@
-<ul id="cat_nav">
-            	<li><a href="my_dashboard1.php" id="active"><span class="icon-cart"></span> MY ORDERS</a></li>
-                <li><a href="personal_information.php"><span class="icon-contacts"> </span> ACCOUNT SETTINGS</a></li>
-                <li style="text-align:center;font-size:13px"><a href="personal_information.php"><span class="icon-user-3"></span> Personal Information</a></li>
-                <li style="text-align:center;font-size:13px"><a href="change_password1.php"><span class="icon-lock-open-3"></span> Change Password</a></li>
-            </ul>
+<?php
+if($_SESSION['user_login_session_id'] == '') {
+    header ("Location: logout.php");
+} 
+?>
+<ul id="cat_nav" style="border:1px solid #ddd;;padding:0px">
+	<li style="font-size:12px"><a href="my_dashboard.php" <?php if($page_name=="my_dashboard.php" || $page_name=="order_details.php") { echo "id=active"; } ?> ><i class="icon-cart"></i> MY ORDERS</a></li>
+    <li style="font-size:12px"><a href="personal_information.php"><i class="icon-address-book"></i> ACCOUNT SETTINGS</a></li>
+    <li style="text-align:center;font-size:13px"><a href="personal_information.php" <?php if($page_name=="personal_information.php") { echo "id=active"; } ?>><i class="icon-user"></i> Personal Information</a></li>
+    <li style="text-align:center;font-size:13px"><a href="change_password.php" <?php if($page_name=="change_password.php") { echo "id=active"; } ?>><i class="icon-lock"></i> Change Password</a></li>            
+</ul>
