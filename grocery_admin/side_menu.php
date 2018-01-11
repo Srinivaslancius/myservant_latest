2bb85966e61f53dbc7d871e32a6c2739faa9dc25
srinivaslancius <srinivas@lanciussolutions.com>
2018-01-11 11:06:46 +0530

    changes in side_menu file

--- grocery_admin/side_menu.php
+++ grocery_admin/side_menu.php
@@ -65,13 +65,7 @@
               </a>
             </li>
 
-            <li class="<?php if($page_name == 'advertisements.php' ) { echo "active"; } ?>"><a href="advertisements.php">
-                <span class="menu-icon">
-                  <i class="zmdi zmdi-filter-tilt-shift zmdi-hc-fw"></i>
-                </span>
-                <span class="menu-text">Advertisements</span>
-              </a>
-            </li>
+    
             <li ><a href="#">
                 <span class="menu-icon">
                   <i class="zmdi zmdi-blogger zmdi-hc-fw"></i>
@@ -88,11 +82,12 @@
             </li>
             <li class="<?php if($page_name == 'grocery_banners.php') { echo "active"; } ?>"><a href="grocery_banners.php">
                 <span class="menu-icon">
-                  <i class="zmdi zmdi-image zmdi-hc-fw"></i>
+                  <i class="zmdi zmdi-info zmdi-hc-fw"></i>
                 </span>
                 <span class="menu-text">Banners</span>
               </a>
             </li>
+  
             <li class="<?php if($page_name == 'grocery_tags.php') { echo "active"; } ?>"><a href="grocery_tags.php">
                 <span class="menu-icon">
                   <i class="zmdi zmdi-link zmdi-hc-fw"></i>
@@ -182,7 +177,7 @@
                 <span class="menu-text">Packages</span>
               </a>
             </li>
-            
+
             <li><a href="#">
                 <span class="menu-icon">
                   <i class="zmdi zmdi-dot-circle-alt"></i>
