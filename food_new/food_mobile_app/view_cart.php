adb11232018835310cd7d6c8e7d534e10b61d3c2
Srinivaslancius <srinivas@lanciussolutions.com>
2018-01-21 10:06:31 +0530

    new chanegs

--- food_new/food_mobile_app/view_cart.php
+++ food_new/food_mobile_app/view_cart.php
@@ -13,27 +13,27 @@ if (isset($_REQUEST['userId'])  ) {
 	if ($getCartFoodData->num_rows > 0) {
 
 		while($row = $getCartFoodData->fetch_assoc()) {
-		$lists = array();
-		$lists["cartId"] = $row["id"];	
-		$lists["productId"]    = $row["food_item_id"];
-		$lists["itemQuantity"] = $row["item_quantity"];	
-		$lists["itemPrice"] = $row["item_price"];	
-		$lists["categoryId"] = $row["food_category_id"];
-		$lists["restaurantId"] = $row["restaurant_id"];
-		$proName= getIndividualDetails('food_products','id',$row['food_item_id']);
-		$lists["productName"] = $proName['product_name'];		
+			$lists = array();
+			$lists["cartId"] = $row["id"];	
+			$lists["productId"]    = $row["food_item_id"];
+			$lists["itemQuantity"] = $row["item_quantity"];	
+			$lists["itemPrice"] = $row["item_price"];	
+			$lists["categoryId"] = $row["food_category_id"];
+			$lists["restaurantId"] = $row["restaurant_id"];
+			$restName= getIndividualDetails('food_vendors','id',$row['restaurant_id']);
+			$lists["restaurantName"] = $restName["restaurant_name"];
+			$proName= getIndividualDetails('food_products','id',$row['food_item_id']);
+			$lists["productName"] = $proName['product_name'];		
+			$getPriceDetails = getIndividualDetails('food_product_weight_prices','weight_type_id',$row["item_weight_type_id"]);	
+			$getWeights = getIndividualDetails('food_product_weights','id',$getPriceDetails['weight_type_id']);
+			$lists["weightTypeId"] =  $row['item_weight_type_id'];
+	    	$lists["weightType"] =  $getWeights['weight_type'];	
 
-		$getPriceDetails = getIndividualDetails('food_product_weight_prices','product_id',$row["food_item_id"]);
-		$lists["priceTypeId"] =  $getPriceDetails['id'];
-		$lists["price"] =  round($getPriceDetails['admin_price']);
-		$getWeights = getIndividualDetails('food_product_weights','id',$getPriceDet['weight_type_id']);
-		$lists["weightTypeId"] =  $getWeights['id'];
-    	$lists["weightType"] =  $getWeights['weight_type'];	
-
-		array_push($response["lists"], $lists);
-	}
+			array_push($response["lists"], $lists);
+		}
 	$response["success"] = 0;
 	$response["message"] = "Success";
+
 	} else {
 
 		$response["success"] = 1;
