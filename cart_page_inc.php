bc5c1abb55dc423ba20b97c014ca297d41d6bbab
Srinivaslancius <srinivas@lanciussolutions.com>
2018-01-22 17:33:55 +0530

    done design changes

--- cart_page_inc.php
+++ cart_page_inc.php
@@ -31,7 +31,7 @@ if(isset($_SESSION['user_login_session_id']) && $_SESSION['user_login_session_id
 $cartTotal = 0;
 
 echo '<div class="row">
-	<div class="col-lg-8">
+	<div class="col-lg-12">
 		<div class="flat-row-title style1">
 			<h3>Shopping Cart</h3>
 		</div>
@@ -42,7 +42,7 @@ echo '<div class="row">
 						<th>Product</th>
 						<th>Quantity</th>
 						<th>Total</th>
-						<th></th>
+						<th>Action</th>
 					</tr>
 				</thead>
 				<tbody>';
@@ -54,14 +54,33 @@ echo '<div class="row">
 					$img = $base_url . 'grocery_admin/uploads/product_images/'.$getProductImage['image'];
 					echo'<tr>
 						<td>
+						<div class="row">
+										<div class="col-sm-3">
 							<div class="img-product">
 								<img src="'.$img.'" alt="">
 							</div>
+							</div>
+							
+										<div class="col-sm-3">
 							<div class="name-product">
 								'.wordwrap($getProductName['product_name'],25,"<br>\n").'
 							</div>
+							</div>
+							<div class="col-sm-3">
+							<div class="quanlity-box">
+											<div class="colors">
+											<select>
+											<option value="96">500 - Rs : 80</option>
+																				  
+												</select>
+											</div>
+									
+											</div>
+											</div>
+							<div class="col-sm-3">
 							<div class="price">
 								 Rs . '.$getCartItems['product_price'].'
+							</div>
 							</div>
 							<div class="clearfix"></div>
 						</td>
@@ -88,7 +107,9 @@ echo '<div class="row">
 			</table>
 		</div>
 	</div>
-	<div class="col-lg-4">
+	<div class="col-lg-7">
+	</div>
+	<div class="col-lg-5">
 	    <div class="cart-totals">
 	        <h3>Cart Totals</h3>
 	        <form action="#" method="get" accept-charset="utf-8">
