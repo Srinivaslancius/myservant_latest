86bc38fe989e50aa0d4839f8b8b9b12f461f966d
srinivaslancius <srinivas@lanciussolutions.com>
2018-01-23 18:46:15 +0530

    changed banners and brands pages in admin panel

--- grocery_admin/manage_brands.php
+++ grocery_admin/manage_brands.php
@@ -131,7 +131,7 @@
                                     <td><?php if ($row['lkp_status_id']==0) { echo "<span class='label label-outline-success check_active open_cursor' data-incId=".$row['id']." data-status=".$row['lkp_status_id']." data-tbname='grocery_brands'>Active</span>" ;} else { echo "<span class='label label-outline-info check_active open_cursor' data-status=".$row['lkp_status_id']." data-incId=".$row['id']." data-tbname='grocery_brands'>In Active</span>" ;} ?></td>
                                     <td> <a href="edit_grocery_brands.php?brand_id=<?php echo $row['id']; ?>"><i class="zmdi zmdi-edit"></i></a> &nbsp; <!-- <a href="delete.php?id=<?php echo $row['id']; ?>&table=<?php echo "grocery_brands" ?>"><i class="zmdi zmdi-delete zmdi-hc-fw" onclick="return confirm('Are you sure you want to delete?')"></i></a> --></td>
                                 </tr>
-                                <?php } ?>
+                                <?php $i++; } ?>
                             </tbody>
                         </table>
                     </div>
